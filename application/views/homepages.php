081c264dfe4ae4ae0935b7a5921d0216f60d7dde
Denise-19 <denisearuan19@gmail.com>
2021-11-27 21:07:33 +0700

    initial commit

---
+++ application/views/homepages.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <!-- Navbar content -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <img src="<?php echo base_url();?>assets/images/satu.png" width="50" height="50">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav position-absolute top-50 start-50 translate-middle"></div>
+                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="#">Blog</a>
+                    <a class="nav-link" href="#">Galery</a>
+                    <a class="nav-link" href="#">Program</a>
+                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="#">sign Up</a>
+                </div>
+            </div>
+            </div>
+        </nav>
+    </nav>
+    <div class="row">
+        <div class="col-6">
+            <h1>BAKTI MILLENIAL</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit
+                anim id est laborum.</p>
+            <button type="button" class="btn btn-warning">Warning</button>
+        </div>
+
+        <div class="col-2">
+            <img src="<?php echo base_url();?>assets/images/satu.png" width="500" height="500">
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="position translate-middle">Projects</div>
+        <div class="col-4">
+            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="card-title">
+                    BAKTI MILLENIAL #3
+                </div>
+                <div class="card-image">
+                    <img src="<?php echo base_url();?>assets/images/satu.png" width="200" height="200">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Secondary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+                <button type="button" class="btn btn-success">Secondary</button>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="card-title">
+                    BAKTI MILLENIAL #3
+                </div>
+                <div class="card-image">
+                    <img src="<?php echo base_url();?>assets/images/satu.png" width="200" height="200">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Secondary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+                <button type="button" class="btn btn-success">Secondary</button>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="card-title">
+                    BAKTI MILLENIAL #3
+                </div>
+                <div class="card-image">
+                    <img src="<?php echo base_url();?>assets/images/satu.png" width="200" height="200">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Secondary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+                <button type="button" class="btn btn-success">Secondary</button>
+            </div>
+        </div>
+    </div>
+    <div class="card text-white bg-secondary mb-3" style="full">
+        <div class="row">
+            <div class="col-6">
+                <h1>BAKTI MILLENIAL</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                    eu
+                    fugiat
+                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit
+                    anim id est laborum.</p>
+            </div>
+            <div class="col-2">
+                <img src="<?php echo base_url();?>assets/images/satu.png" width="400" height="400">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-2">
+            <img src="<?php echo base_url();?>assets/images/satu.png" width="200" height="200">
+        </div>
+        <div class="col-8">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                eu
+                fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit
+                anim id est laborum.</p>
+        </div>
+    </div>
+    <div class="card text-white bg-secondary mb-3" style="full">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+            et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip
+            ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu
+            fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit
+            anim id est laborum.</p>
+        <div class="flex">
+            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
+            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
+            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
+        </div>
+    </div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
