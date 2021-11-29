617de775162dbe25c95f07b55fc14b8bb74c3bb6
Denise-19 <denisearuan19@gmail.com>
2021-11-29 17:39:14 +0700

    update homepages

--- application/views/homepages.php
+++ application/views/homepages.php
@@ -7,26 +7,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
-    <title>Hello, world!</title>
+
+    <title>Bakti Millenial</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <nav class="navbar navbar-light" style="background-color: #8FEABB;">
         <!-- Navbar content -->
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
-                <img src="<?php echo base_url();?>assets/images/satu.png" width="50" height="50">
+                <img src="<?php echo base_url();?>assets/images/LOGO.png" width="100" height="50">
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav position-absolute top-50 start-50 translate-middle"></div>
-                    <a class="nav-link" href="#">Home</a>
-                    <a class="nav-link" href="#">Blog</a>
-                    <a class="nav-link" href="#">Galery</a>
-                    <a class="nav-link" href="#">Program</a>
-                    <a class="nav-link" href="#">Contact</a>
-                    <a class="nav-link" href="#">sign Up</a>
+                    <a class="nav-link rounded-pill" href="#">Home</a>
+                    <a class="nav-link rounded-pill" href="#">Blog</a>
+                    <a class="nav-link rounded-pill" href="#">Galery</a>
+                    <a class="nav-link rounded-pill" href="#">Program</a>
+                    <a class="nav-link rounded-pill" href="#">Contact</a>
+                    <a class="nav-link rounded-pill" href="#">sign Up</a>
                 </div>
             </div>
             </div>
@@ -43,18 +51,19 @@
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                 mollit
                 anim id est laborum.</p>
-            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="rounded-pill btn-warning">Read More</button>
         </div>
 
-        <div class="col-2">
+        <div class=" col-2">
             <img src="<?php echo base_url();?>assets/images/satu.png" width="500" height="500">
 
         </div>
     </div>
+
+    <!-- <div class="position translate-middle">Projects</div>
     <div class="row">
-        <div class="position translate-middle">Projects</div>
         <div class="col-4">
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+            <div class="card-title text-white bg-warning " style="max-width: 18rem;">
                 <div class="card-title">
                     BAKTI MILLENIAL #3
                 </div>
@@ -103,22 +112,68 @@
                 <button type="button" class="btn btn-success">Secondary</button>
             </div>
         </div>
+    </div> -->
+    <div class="container pt-2 text-center">
+        <h2>Projects</h2>
+        <div class="row pt-2">
+            <div class="col-sm-4">
+                <div class="card" style="width:400px">
+                    <h4 class="card-title">Bakti Millenial #3</h4>
+                    <img class="card-img-top" src="<?php echo base_url();?>assets/images/satu.png" alt="Card image"
+                        style="width:100%">
+                    <div class="card-body">
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer
+                        </p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="width:400px">
+                    <h4 class="card-title">Bakti Millenial #3</h4>
+                    <img class="card-img-top" src="<?php echo base_url();?>assets/images/satu.png" alt="Card image"
+                        style="width:100%">
+                    <div class="card-body">
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer
+                        </p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="width:400px">
+                    <h4 class="card-title">Bakti Millenial #3</h4>
+                    <img class="card-img-top" src="<?php echo base_url();?>assets/images/satu.png" alt="Card image"
+                        style="width:100%">
+                    <div class="card-body">
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer
+                        </p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card text-white bg-secondary mb-3" style="full">
+
+    <div class="container-fluid pt-2 p-3 my-3 bg-primary text-white">
         <div class="row">
             <div class="col-6">
                 <h1>BAKTI MILLENIAL</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                    labore
                     et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip
                     ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore
                     eu
                     fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt
                     mollit
                     anim id est laborum.</p>
+                <a href="#" class="btn btn-warning">Read more</a>
             </div>
             <div class="col-2">
                 <img src="<?php echo base_url();?>assets/images/satu.png" width="400" height="400">
@@ -126,40 +181,56 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-2">
-            <img src="<?php echo base_url();?>assets/images/satu.png" width="200" height="200">
+    <div class="row pt-2">
+        <div class="col-6 pt-0">
+            <img src="<?php echo base_url();?>assets/images/satu.png" style="width:50%">
         </div>
-        <div class="col-8">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip
-                ex
-                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-                eu
-                fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit
-                anim id est laborum.</p>
+        <div class="col-6 pt-0">
+            <div class="card-body">
+                <h4 class="card-title">Fully Funded</h4>
+                <p class="card-text">Dibiayai penuh oleh Yayasan Anak Millenial Indonesia</p>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Partial Funded</h4>
+                <p class="card-text">Diberikan potongan biaya jalur beasiswa.</p>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Self Funded</h4>
+                <p class="card-text">Keseluruhan biaya ditanggung pribadi.</p>
+            </div>
         </div>
     </div>
-    <div class="card text-white bg-secondary mb-3" style="full">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-            et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip
-            ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-            eu
-            fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit
-            anim id est laborum.</p>
+    <div class="container-fluid p-5 bg-primary text-center text-white">
+        <p>Ikuti kami di sosial media untuk mendapatkan informasi mengenai event, volunteer, bakti millenial, dan
+            perkembangan Bakti Millenial yang lainnya.</p>
         <div class="flex">
-            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
-            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
-            <img src="<?php echo base_url();?>assets/images/satu.png" width="70" height="70">
+            <img src="<?php echo base_url();?>assets/images/satu.png" class="rounded-circle" width="70" height="70">
+            <img src="<?php echo base_url();?>assets/images/satu.png" class="rounded-circle" width="70" height="70">
+            <img src="<?php echo base_url();?>assets/images/satu.png" class="rounded-circle" width="70" height="70">
+        </div>
+    </div>
+    <div>
+        <div class="container-fluid text-center text-white" style="background-color: #184D47;">
+            <div class="row">
+                <div class="col-4 pt-3">
+                    <img src="<?php echo base_url();?>assets/images/LOGO.png" width="150" height="80">
+                </div>
+                <div class="col-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Contact Us</h4>
+                        <p class="card-text">Dibiayai penuh oleh Yayasan Anak Millenial Indonesia</p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Follow Us</h4>
+                        <p class="card-text">Dibiayai penuh oleh Yayasan Anak Millenial Indonesia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid text-center text-white bg-dark">
+            <p>Copyrigth @2021 Bakti Millenial | Theme by Bakti Millenial | More </p>
         </div>
     </div>
 
